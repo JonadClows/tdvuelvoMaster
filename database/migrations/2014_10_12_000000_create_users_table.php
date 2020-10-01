c308490e758f2cacb6d9b3b705a57e671aa111a9
Jean Hernández <jeanchernandez@gmail.com>
2020-10-01 15:33:45 -0500

    Quitar atributo unique al campo cédula (corrección momentánea)

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('cedula')->unique();
+            $table->string('cedula');//->unique();
             $table->string('name');
             $table->string('telefono')->nullable();
             $table->string('id_ciudad');
