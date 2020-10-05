3de8712dbd38d118d485670f4ca8b688cfc30e84
Adan <acabanilla@grupo-link.com>
2020-10-05 14:32:37 -0500

    Lista Bancos

--- database/seeders/BancoSeeder.php
+++ database/seeders/BancoSeeder.php
@@ -14,32 +14,35 @@ class BancoSeeder extends Seeder
      */
     public function run()
     {
+        Banco::create(['name' => 'Banco Pichincha']);
+        Banco::create(['name' => 'Banco Produbanco Grupo Promerica']);
         Banco::create(['name' => 'Banco Bolivariano']);
-        Banco::create(['name' => 'Banco Central del Ecuador']);
-        Banco::create(['name' => 'Banco Centro Mundo']);
-        Banco::create(['name' => 'Banco Continental']);
-        Banco::create(['name' => 'Banco de Guayaquil']);
-        Banco::create(['name' => 'Banco de Loja']);
-        Banco::create(['name' => 'Banco de Machala']);
-        Banco::create(['name' => 'Banco del Austro']);
-        Banco::create(['name' => 'Banco del Estado']);
-        Banco::create(['name' => 'Banco del Pacífico']);
-        Banco::create(['name' => 'Banco DELBANK']);
+        Banco::create(['name' => 'Banco Diners Club']);
+        Banco::create(['name' => 'Banco Austro']);
+        Banco::create(['name' => 'Banco Pacífico']);
+        Banco::create(['name' => 'Banco Guayaquil']);
+        Banco::create(['name' => 'Banco Loja']);
         Banco::create(['name' => 'Banco General Rumiñahui']);
         Banco::create(['name' => 'Banco Internacional']);
-        Banco::create(['name' => 'Banco MM Jaramillo Arteaga']);
-        Banco::create(['name' => 'Banco Mundial Ecuador']);
-        Banco::create(['name' => 'Banco ProCredit Ecuador']);
-        Banco::create(['name' => 'Banco Territorial']);
-        Banco::create(['name' => 'Banco Universal Unibanco']);
-        Banco::create(['name' => 'Banred']);
-        Banco::create(['name' => 'Filanbanco']);
-        Banco::create(['name' => 'Grupo Financiero Producción - Produbanco']);
-        Banco::create(['name' => 'Lloyds Tsb Bank']);
-        Banco::create(['name' => 'Mutualista Benalcázar']);
-        Banco::create(['name' => 'Mutualista Pichincha']);
-        Banco::create(['name' => 'Pacific National Bank']);
-        Banco::create(['name' => 'Servipagos']);
-        Banco::create(['name' => 'Todo1']);
+        Banco::create(['name' => 'Banco BanEcuador']);
+        Banco::create(['name' => 'Banco Machala']);
+        Banco::create(['name' => 'Banco Solidario']);
+        Banco::create(['name' => 'Banco Coopnacional']);
+        Banco::create(['name' => 'Banco Procredit']);
+        Banco::create(['name' => 'Banco Comercial de Manabí']);
+        Banco::create(['name' => 'Banco Delbank']);
+        Banco::create(['name' => 'Banco Desarrollo de los Pueblos']);
+        Banco::create(['name' => 'Banco Finca']);
+        Banco::create(['name' => 'Banco Visionfund']);
+        Banco::create(['name' => 'Banco Dmiro']);
+        Banco::create(['name' => 'Banco Citibank']);
+        Banco::create(['name' => 'Banco Litoral']);
+        Banco::create(['name' => 'Banco Amazonas']);
+        Banco::create(['name' => 'Banco Capital']);
+        Banco::create(['name' => 'Banco 29 de Octubre']);
+        Banco::create(['name' => 'Banco Juventud Ecuatoriana Progresista']);
+        Banco::create(['name' => 'Banco Andalucia']);
+        Banco::create(['name' => 'Banco Coop. Policía Nacional']);
+
     }
 }
